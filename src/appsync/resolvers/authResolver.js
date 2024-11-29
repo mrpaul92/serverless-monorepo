ef2df32622dec74b69c41e7f4275d6c63d845674
Suman <mrpaul92@gmail.com>
2024-11-29 17:48:54 +0530

    formatting

--- src/appsync/resolvers/authResolver.js
+++ src/appsync/resolvers/authResolver.js
@@ -1,2 +1,16 @@
-export function request(ctx) {}
-export function response(ctx) {}
+export function request(ctx) {
+  console.log("ctx in request", ctx);
+  const { identity } = ctx;
+  //   if (!identity || !identity.sub) {
+  //     return runtime.earlyReturn({});
+  //   }
+  return {
+    operation: "Invoke",
+    payload: ctx,
+  };
+}
+export function response(ctx) {
+  console.log("ctx in response", ctx);
+  const { error, result } = ctx;
+  return ctx.result;
+}
